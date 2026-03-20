33f33d5749deeb5b2cbf023405ffbb37280b2f5c
An01n <hanzy@email.sdu.edu.cn>
2026-03-20 15:55:30 +0800

    zip res with datetime

--- zip_res.sh
+++ zip_res.sh
@@ -1,15 +1,14 @@
+# /bin/bash
+
 zip_tar=''
-
+datetime=$(date +%Y%m%d_%H%M%S)
 if grep -q 'enable_bank=true' ./run.sh; then
-    zip_tar=res_preempt.zip
+    zip_tar="res_preempt_${datetime}.zip"
 else
-    zip_tar=res_caravan.zip
+    zip_tar="res_caravan_${datetime}.zip"
 fi
-
 echo 'ziping ' $zip_tar
-
-rm ./$zip_tar
+rm -f ./$zip_tar
 zip -r $zip_tar log
-
 cp ./$zip_tar /disc/home/dy/caddy/site
-echo 'copyed ' $zip_tar 'to site'
+echo 'copyed ' $zip_tar 'to download site'
